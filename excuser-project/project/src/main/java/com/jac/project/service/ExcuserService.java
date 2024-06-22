75a1ba6cfad6e9131b29e31ad3eca5572d0ec7aa
iasssy <iana.setrakova@johnabbottcollege.net>
2024-06-22 08:35:48 -0400

    edited adapter, controller, application.properties

--- excuser-project/project/src/main/java/com/jac/project/service/ExcuserService.java
+++ excuser-project/project/src/main/java/com/jac/project/service/ExcuserService.java
@@ -1,7 +1,16 @@
 package com.jac.project.service;
 
+import com.jac.project.adapter.ExcuserAdapter;
+import com.jac.project.model.Excuse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExcuserService {
+    @Autowired
+    ExcuserAdapter excuserAdapter;
+
+    public Excuse getRandomExcuse(){
+        return excuserAdapter.getRandomExcuse();
+    }
 }
